00b37a8a4ce6bbb8a87726a85cb00969c6467d1b
Michele <53474707+mifrugo@users.noreply.github.com>
2021-03-08 14:46:30 +0100

    added comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,32 @@
 Rails.application.routes.draw do
+  # User routes
   devise_for :users
+
+  # Homepage
   root to: 'pages#home'
 
-  get     '/yatchs',      to: 'yatchs#index'
-  get     '/yatchs/:id',  to: 'yatchs#show'
-  post    '/yatchs/:id',  to: 'yatchs#book'
-  patch   '/yatchs/:id',  to: 'yatchs#edit'
-  delete  '/yatchs/:id',  to: 'yatchs#destroy'
+  # Yatchs listing
+  get     '/yatchs',              to: 'yatchs#index'
+  # Single yatch
+  get     '/yatchs/:id',          to: 'yatchs#show'
+  # Book a yatch
+  post    '/yatchs/:id',          to: 'yatchs#book'
+  # Update listing
+  patch   '/yatchs/:id',          to: 'yatchs#edit'
+  # Remove listing
+  delete  '/yatchs/:id',          to: 'yatchs#destroy'
+  # Update review
+  patch   '/reviews/:id',         to: 'reviews#update'
 
-  patch   '/reviews/:id', to: 'reviews#update'
+  # Add to favorite
+  #post    '/yatchs/:id/favorite', to: 'favorite#add'
+  # Delete favorite
+  #delete '/favorites/:id',       to: 'favorites#destroy'
 
+  # Conversations list
+  #get '/messages',               to: 'messages#list'
+  # Single conversation
+  #get '/messages/:id',           to: 'messages#show'
+  # Add new message to conversation
+  #post '/messages/:id',          to: 'messages#add'
 end
