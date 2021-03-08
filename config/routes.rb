7419f0fb918c3c2fcdfb55ead7b3e6b88c994f75
Michele <53474707+mifrugo@users.noreply.github.com>
2021-03-08 14:35:22 +0100

    edited controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  get     '/yatchs'      to: 'yatchs#index'
-  get     '/yatchs/:id'  to: 'yatchs#show'
-  post    '/yatchs/:id'  to: 'yatchs#book'
-  patch   '/yatchs/:id'  to: 'yatchs#edit'
-  delete  '/yatchs/:id'  to: 'yatchs#destroy'
+  get     '/yatchs',      to: 'yatchs#index'
+  get     '/yatchs/:id',  to: 'yatchs#show'
+  post    '/yatchs/:id',  to: 'yatchs#book'
+  patch   '/yatchs/:id',  to: 'yatchs#edit'
+  delete  '/yatchs/:id',  to: 'yatchs#destroy'
 
-  patch   '/reviews/:id' to: 'reviews#update'
+  patch   '/reviews/:id', to: 'reviews#update'
 
 end
