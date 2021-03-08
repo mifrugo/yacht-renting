47238c26a557ad38552e7fe776a21abf157db07f
Michele <53474707+mifrugo@users.noreply.github.com>
2021-03-09 00:30:52 +0100

    added redis cache

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,29 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
+  after_save :wipe_cache
+
+  def self.serialize_from_session(key, salt)
+    user = Rails.cache.fetch("user:#{key.first}") do
+      to_adapter.get(key)
+    end
+
+    return user if user && user.authenticatable_salt == salt
+
+    super
+  end
+
   def navbar_avatar(size: 40)
     if avatar.attached?
       cl_image_tag(avatar.key, class: 'avatar')
     else
       lavatar_tag(first_name[0] << last_name[0], size, { class: 'avatar' })
     end
+  end
+
+  private
+
+  def wipe_cache
+    Rails.cache.delete("user:#{id}")
   end
 end
