afdac3fa9a145972b864a9446e4923c1b66e1cf8
Besmir Gjata <besigjata@gmail.com>
2021-03-09 16:15:55 +0100

    pundit feature

--- app/controllers/yachts_controller.rb
+++ app/controllers/yachts_controller.rb
@@ -1,10 +1,17 @@
 class YachtsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
+  before_action :set_yacht, only: %i[show edit update delete]
 
   def index
+    @yachts = policy_scope(Yacht)
   end
 
   def show
+  end
+
+  def new
+    @yacht = Yacht.new
+    authorize @yacht
   end
 
   def book
@@ -14,11 +21,31 @@ class YachtsController < ApplicationController
   end
 
   def create
+    @yacht = Yacht.new(yacht_params)
+    @yacht.user = current_user
+    authorize @yacht
+
+    if @yacht.save
+      redirect_to @yacht, notice: "Yacht was successfully created"
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+
+  def set_yacht
+    @yacht = Yacht.find(params[:id])
+    authorize @yacht
+  end
+
+  def yacht_params
+    params.require(@yacht).permit(:title, :description, :booking_type)
   end
 end
