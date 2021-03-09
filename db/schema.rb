250ca9b0e9b6ddfa321ff792619804c022a9b13d
Michele <53474707+mifrugo@users.noreply.github.com>
2021-03-09 22:38:37 +0100

    initial migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_154715) do
+ActiveRecord::Schema.define(version: 2021_03_09_213815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,10 +44,22 @@ ActiveRecord::Schema.define(version: 2021_03_09_154715) do
     t.index ["yacht_id"], name: "index_equipment_on_yacht_id"
   end
 
+  create_table "equipment_types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "locations", force: :cascade do |t|
     t.string "name"
     t.decimal "lat"
     t.decimal "long"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "service_types", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
