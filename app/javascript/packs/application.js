9b264fa58d401c871d2fd4c7d1dd5f6400edd4f2
Michele <53474707+mifrugo@users.noreply.github.com>
2021-03-10 15:12:16 +0100

    fix for heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,12 +16,15 @@ window.$ = $;
 import { removeAlert } from '../components/init_removeAlert'
 import { renderMap } from '../components/init_mapNewListing'
 import { initTimeAgo } from '../components/init_timeago'
+import { navBar } from '../components/init_navbar'
 
 import 'bootstrap-select/dist/js/bootstrap-select.min.js';
 
 document.addEventListener('turbolinks:load', () => {
   removeAlert();
   initTimeAgo();
+
+  navBar();
 
   $('.selectpicker').selectpicker();
 
